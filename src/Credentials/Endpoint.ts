f4ff6cd63e15b8c365ca13ac2b01f0b62fbe06d8
Quinn Turner <quinnturnertech@gmail.com>
2023-09-22 11:18:18 -0300

    feat: Allow proxying Intacct API calls in local development

--- src/Credentials/Endpoint.ts
+++ src/Credentials/Endpoint.ts
@@ -26,6 +26,8 @@ export default class Endpoint {
 
     public static readonly ENDPOINT_URL_ENV_NAME = "INTACCT_ENDPOINT_URL";
 
+    public static readonly ALLOW_NON_INTACCT_ENDPOINT_URL_ENV_NAME = "INTACCT_ALLOW_NON_INTACCT_ENDPOINT_URL";
+
     public static readonly DOMAIN_NAME = "intacct.com";
 
     public static readonly FULL_QUALIFIED_DOMAIN_NAME = Endpoint.DOMAIN_NAME + ".";
@@ -41,18 +43,29 @@ export default class Endpoint {
         return this._url;
     }
 
+    private allowNonIntacctEndpointUrl: boolean;
+
+    private verifyEndpointUrl(address: string): void | never {
+        const parsedUrl = url.parse(address);
+        if (!Endpoint.isDomainValid(parsedUrl.hostname) && !this.allowNonIntacctEndpointUrl) {
+            throw new Error("Endpoint URL is not a valid " + Endpoint.DOMAIN_NAME + " domain name.");
+        }
+    }
+
     set url(address: string) {
         if (address == null || address === "") {
             address = Endpoint.DEFAULT_ENDPOINT;
         }
 
-        const parsedUrl = url.parse(address);
-        if (!Endpoint.isDomainValid(parsedUrl.hostname)) {
-            throw new Error("Endpoint URL is not a valid " + Endpoint.DOMAIN_NAME + " domain name.");
-        }
+        this.verifyEndpointUrl(address);
         this._url = address;
     }
     constructor(config: ClientConfig) {
+        if (config.allowNonIntacctEndpointUrl == null) {
+            this.allowNonIntacctEndpointUrl = process.env[Endpoint.ALLOW_NON_INTACCT_ENDPOINT_URL_ENV_NAME] === "true";
+        } else {
+            this.allowNonIntacctEndpointUrl = config.allowNonIntacctEndpointUrl;
+        }
         if (config.endpointUrl == null) {
             this.url = process.env[Endpoint.ENDPOINT_URL_ENV_NAME];
         } else {
